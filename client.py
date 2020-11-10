ffdad6da29236d0e7817cc5b853b955a0e633adb
Dev Lathiya <53378552+deus09@users.noreply.github.com>
2020-11-10 16:43:49 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,54 +1,65 @@
-from socket import socket
-from zlib import decompress
-
+import socket
+import zlib
 import pygame
 
-WIDTH = 1366
-HEIGHT = 768
+WIDTH = 768 # default
+HEIGHT = 432 # default
+HOST = "0.0.0.0" # default
+PORT = 9999 # default
 
+def setup():
+    global WIDTH 
+    global HEIGHT
+    global HOST
+    print()
+    print("To connect to server, complete the setup: ")
+    print()
+    HOST = input("Enter the IP address of server (IPv4): ")
+    print()
+    print("Enter the dimension [should be same as server]")
+    WIDTH = input("WIDTH: ")
+    HEIGHT = input("HEIGHT: ")
 
-def recvall(conn, length):
-    """ Retreive all pixels. """
-
-    buf = b''
-    while len(buf) < length:
-        data = conn.recv(length - len(buf))
+def getAll(conn, length):
+    buffer = b''
+    while len(buffer) < length:
+        data = conn.recv(length - len(buffer))
         if not data:
             return data
-        buf += data
-    return buf
+        buffer += data
+    return buffer
 
-
-def main(host='127.0.0.1', port=5000):
+def connect_to_server():
     pygame.init()
-    screen = pygame.display.set_mode((WIDTH, HEIGHT))
-    clock = pygame.time.Clock()
-    watching = True    
+    screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
+    clock = pygame.time.Clock()   
 
-    sock = socket()
-    sock.connect((host, port))
+    ADDR = (HOST, PORT)
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
+        server.connect(ADDR)
+        watching = True
         while watching:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     watching = False
                     break
+            size_len = int.from_bytes(server.recv(1), byteorder='big')
 
-            # Retreive the size of the pixels length, the pixels length and pixels
-            size_len = int.from_bytes(sock.recv(1), byteorder='big')
-            size = int.from_bytes(sock.recv(size_len), byteorder='big')
-            pixels = decompress(recvall(sock, size))
-
-            # Create the Surface from raw pixels
+            size = int.from_bytes(server.recv(size_len), byteorder='big')
+            
+            pixels = zlib.decompress(getAll(server, size))
+            
             img = pygame.image.fromstring(pixels, (WIDTH, HEIGHT), 'RGB')
-
-            # Display the picture
+            
             screen.blit(img, (0, 0))
+            
             pygame.display.flip()
+            
             clock.tick(60)
     finally:
-        sock.close()
-
+        server.close()
 
 if __name__ == '__main__':
-    main()
+    setup()
+    connect_to_server()
